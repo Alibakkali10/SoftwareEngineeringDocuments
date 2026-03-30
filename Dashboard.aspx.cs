d1654bf205dea6a2fec4ea2d618090303511de99
mouadlg23658 <mouadelg464@gmail.com>
2026-03-29 23:02:10 -0400

    Add files via upload

---
+++ Dashboard.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hotel
+{
+    public partial class Dashboard : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnReserve_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Reserve.aspx");
+        }
+
+        protected void btnReservations_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Reservations.aspx");
+        }
+    }
+}
